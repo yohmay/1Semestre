6228029409286cc3368d616b83ebd0ca344a110b
Yohana Mayra <yohanamayra@gmail.com>
2023-06-08 19:37:41 -0300

    Java

---
+++ Prova/TesteClasse.java
@@ -0,0 +1,9 @@
+/* public class TesteClasse {
+    public static void main(String args[]) {
+        classe qualquer = new classe();
+        qualquer.item1 = "Teste";
+        System.out.println(qualquer.item1);
+    }
+    
+}
+*/
